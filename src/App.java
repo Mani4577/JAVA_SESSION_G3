600c5dd45bc63c409dbd3dbee24eb7c5e8b73d3e
Pavani023 <145253494+Mani4577@users.noreply.github.com>
2025-01-04 11:33:05 +0530

    message

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     int m = 10;
+     int n = 3;
+    if (m>n){
+        System.out.print("m is greater");
+    
+    }else{
+        System.out.println("n is greater ");
+    } 
+
+
+}
+}
+
+
