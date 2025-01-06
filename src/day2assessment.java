3b9564a541a5bc97f67c25cd4f8bfea6fbdc5dc9
SAIMANI45 <145253494+Mani4577@users.noreply.github.com>
2025-01-06 21:12:44 +0530

    Update day2assessment.java

---
+++ src/day2assessment.java
@@ -0,0 +1,14 @@
+public class day2assessment {
+    public static void main(String[] args) {
+        int n=5;
+        if(n>0){
+            System.out.println("n is positive");
+        }
+        else if (n<0){
+            System.out.println("n is negative");
+        }
+        else{
+            System.err.println("n is zero");
+        }
+    }
+}
